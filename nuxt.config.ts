f2aa6c1b658838fe564e50ac8d693a240a945496
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-14 03:45:25 +0500

    added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,26 +12,22 @@ export default defineNuxtConfig({
       },
       title: siteTitle,
       meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-          hid: "description",
-          name: "description",
-          content: "A awesome developer portfolio design.",
-        },
-        { hid: "og:title", property: "og:title", content: siteTitle },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: "A awesome developer portfolio design.",
-        },
-        { hid: "og:image", property: "og:image", content: "demo-share.jpg" },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: "https://developer-portfolio-v1.netlify.app/",
-        },
-        { name: "theme-color", content: "#010C15" },
+        { name: 'description', content: 'Experienced frontend developer specializing in Vue.js, Electron, and modern web technologies.' },
+        { name: 'keywords', content: 'Frontend Developer, Vue.js, Nuxt.js, Electron.js, JavaScript, Portfolio' },
+        { name: 'author', content: 'Azamjon Abdullayev' },
+
+        // Open Graph
+        { property: 'og:title', content: 'Azamjon Abdullayev – Frontend Engineer' },
+        { property: 'og:description', content: 'Explore Azamjon’s personal projects, technical skills, and contact details.' },
+        { property: 'og:image', content: 'https://senioraka.netlify.app/preview.png' }, 
+        { property: 'og:url', content: 'https://senioraka.netlify.app' },
+        { property: 'og:type', content: 'website' },
+
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'Azamjon Abdullayev – Frontend Developer' },
+        { name: 'twitter:description', content: 'Personal portfolio and contact platform of Azamjon.' },
+        { name: 'twitter:image', content: 'https://senioraka.netlify.app/preview.png' }
       ],
       link: [
         { rel: "manifest", href: "pwa/manifest.json" },
